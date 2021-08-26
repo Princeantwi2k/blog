ab76a7e2593692ad146a1e336ebf00ca820d61ad
princeantwi2k <antwi2k@gmail.com>
2021-08-26 13:58:52 +0800

    added iterms.jsx

--- src/Component/Iterms.jsx
+++ src/Component/Iterms.jsx
@@ -1,27 +1,8 @@
 import React from "react";
-import { Card, Container, Row } from "react-bootstrap";
+
 import "./Phone.css";
 const Iterms = (props) => {
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Card
-            style={{ width: "12rem", border: `none`, margin: `0` }}
-            className="card"
-          >
-            <Card.Img variant="top" src={props.details.imgUrl} />
-            <Card.Body>
-              <Card.Title> {props.details.name}</Card.Title>
-              <Card.Text>
-                <p>{props.details.price}</p>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Row>
-      </Container>
-    </div>
-  );
+  return <></>;
 };
 
 export default Iterms;
