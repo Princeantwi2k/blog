2f85a8d1d4e019a97a2ee030e20b4e23ba67600e
princeantwi2k <antwi2k@gmail.com>
2021-08-26 13:59:11 +0800

    added product.jsx

--- src/Component/Product.jsx
+++ src/Component/Product.jsx
@@ -1,16 +1,40 @@
 import React from "react";
-import Iterm from "./Iterms";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const Product = (props) => {
+const Product = ({ product }) => {
   return (
     <>
-      {props.product.map((product, id) => (
-        <div>
-          <Iterm details={product} key={id} phone={product} />
-        </div>
+      {product.map((product, id) => (
+        <>
+          <Link to={`/blogs/${product.id}`} className="links">
+            <div key={id}>
+              <Card
+                style={{
+                  width: "13.8rem",
+                  border: `none`,
+                  marginBottom: `10px`,
+                }}
+                className="card"
+              >
+                <Card.Img variant="top" src={product.imgUrl} />
+                <Card.Body>
+                  <Card.Title> {product.name}</Card.Title>
+                  <Card.Text>
+                    <p>{product.price}</p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </Link>
+        </>
       ))}
     </>
   );
 };
+const mapStateToProps = (state) => {
+  return { product: state.topsell };
+};
 
-export default Product;
+export default connect(mapStateToProps)(Product);
