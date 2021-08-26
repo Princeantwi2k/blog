5da4a335838e4ce2bf5b1a9addc68e7c7cb5a376
princeantwi2k <antwi2k@gmail.com>
2021-08-26 11:55:51 +0800

    added Phone.jsx

--- src/Phone.jsx
+++ src/Phone.jsx
@@ -5,20 +5,7 @@ const Phone = (props) => {
   return (
     <div>
       <Container>
-        <Row>
-          <Card
-            style={{ width: "12rem", border: `none`, margin: `0` }}
-            className="card"
-          >
-            <Card.Img variant="top" />
-            <Card.Body>
-              <Card.Title> {props.phone.name}</Card.Title>
-              <Card.Text>
-                <p>{props.phone.price}</p>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Row>
+        <Row></Row>
       </Container>
     </div>
   );
