32532f0ea73e4c677c7563b35ed339cf80e8598b
princeantwi2k <antwi2k@gmail.com>
2021-08-26 11:55:28 +0800

    added index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from "react-redux"
+import Store from "./Store/Store"
+
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={Store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
