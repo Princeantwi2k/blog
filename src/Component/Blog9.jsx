98cea73cd10e422742a29627b1baba9c32589105
princeantwi2k <antwi2k@gmail.com>
2021-09-25 15:00:29 +0800

    added blog9.jsx

--- src/Component/Blog9.jsx
+++ src/Component/Blog9.jsx
@@ -2,12 +2,28 @@ import { useParams } from "react-router-dom";
 import { Container, Col, Row } from "react-bootstrap";
 import React from "react";
 import { connect } from "react-redux";
-import { Button } from "react-bootstrap";
+import { Button, Popover, OverlayTrigger } from "react-bootstrap";
+import "./Blog.css";
 
 const Blog9 = ({ speaker }) => {
   const { id } = useParams();
 
   let [product] = speaker.filter((speaker) => speaker.id === id);
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">REACH US ON </Popover.Header>
+      <Popover.Body>
+        <h5>0550300103</h5>
+        <h5>0572211378</h5>
+      </Popover.Body>
+    </Popover>
+  );
+  const Example = () => (
+    <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+      <Button variant="warning">Buy me</Button>
+    </OverlayTrigger>
+  );
 
   return (
     <div className="bloglist d-grid gap-2">
@@ -23,9 +39,7 @@ const Blog9 = ({ speaker }) => {
           <Col sm={8} md={8}>
             <h1 className="items-name">{product.name}</h1>
             <p>{product.price}</p>
-            <Button variant="primary" size="lg">
-              Buy Me{" "}
-            </Button>
+            <Example />
           </Col>
         </Row>
       </Container>
