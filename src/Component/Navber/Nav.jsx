b2a17b550fc6456b777a3d5bc8c690c952a3807a
princeantwi2k <antwi2k@gmail.com>
2021-09-25 15:02:54 +0800

    added Navs

--- src/Component/Navber/Nav.jsx
+++ src/Component/Navber/Nav.jsx
@@ -1,84 +1,58 @@
 import React, { Component } from "react";
-import { Navbar, FormControl, Form, Button } from "react-bootstrap";
-import { NavDropdown, Container, Row, Col } from "react-bootstrap";
+import { Navbar, Form, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import "./Nav.css";
-import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Nav extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState({});
-  };
+
   render() {
     return (
       <>
         <Container fluid className=" fixed-top">
           <Row>
             <Col sm={6} md={12}>
-              <Navbar className="Navbar">
-                <h3
-                  style={{
-                    marginLeft: `10%`,
-                    fontSize: `40px`,
-                    fontWeight: `bolder`,
-                    fontStyle: `italic`,
-                  }}
-                >
-                  Blue
-                  <span style={{ color: `orange`, fontFamily: `sans-serif` }}>
-                    Let
-                  </span>
-                </h3>
-                <Form className="d-flex form" onSubmit={this.handleSubmit}>
-                  <FormControl
-                    type="search"
-                    name="input"
-                    onChange={this.handleChange}
-                    value={this.state}
-                    placeholder="Search product, brands and categories"
-                    className="mr-2 input"
-                    aria-label="Search"
-                  />
-                  <Button variant="warning" size="sm">
-                    Search
-                  </Button>
-                </Form>
-                <NavDropdown
-                  title="Account"
-                  id="navbarScrollingDropdown day"
-                  className="drop"
-                >
-                  <NavDropdown.Item href="#action3">Log In</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    MY Account
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">Oders</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown
-                  title="Help"
-                  id="navbarScrollingDropdown"
-                  className="drop"
-                >
-                  <NavDropdown.Item to="/">Home</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
+              <Navbar expand="lg" variant="light" bg="light">
+                <Container fluid className="navbar">
+                  <Navbar.Brand>
+                    {" "}
+                    <h3
+                      style={{
+                        marginLeft: `10%`,
+                        fontSize: `40px`,
+                        fontWeight: `bolder`,
+                        fontStyle: `italic`,
+                      }}
+                    >
+                      Blue
+                      <span
+                        style={{ color: `orange`, fontFamily: `sans-serif` }}
+                      >
+                        Let
+                      </span>
+                    </h3>
+                  </Navbar.Brand>
+                  <Form className="d-flex ">
+                    <input
+                      type="search"
+                      placeholder="Search ,product ,brands, and categories"
+                      className="mr-2 search"
+                      aria-label="Search"
+                    />
+                    <Button variant="warning">Search</Button>
+                  </Form>
+                  <div className="navlinks">
+                    <Link to="/" className="drop">
+                      HOME
+                    </Link>
+                    <Link to="/login" className="drops">
+                      ACCOUNT
+                    </Link>
+                  </div>
+                </Container>
               </Navbar>
             </Col>
           </Row>
@@ -87,8 +61,5 @@ class Nav extends Component {
     );
   }
 }
-const mapDispatchToProps = (state) => {
-  return { state: state };
-};
 
-export default connect(null, mapDispatchToProps)(Nav);
+export default Nav;
