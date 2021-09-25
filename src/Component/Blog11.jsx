fa42c5fa22ccbe11b044ae1d58e0319a51f486b1
princeantwi2k <antwi2k@gmail.com>
2021-09-25 15:01:01 +0800

    added Blog11.jsx

--- src/Component/Blog11.jsx
+++ src/Component/Blog11.jsx
@@ -2,13 +2,27 @@ import { useParams } from "react-router-dom";
 import { Container, Col, Row } from "react-bootstrap";
 import React from "react";
 import { connect } from "react-redux";
-import { Button } from "react-bootstrap";
-
+import { Button, Popover, OverlayTrigger } from "react-bootstrap";
+import "./Blog.css";
 const Blog11 = ({ baby }) => {
   const { id } = useParams();
 
   let [product] = baby.filter((baby) => baby.id === id);
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">REACH US ON </Popover.Header>
+      <Popover.Body>
+        <h5>0550300103</h5>
+        <h5>0572211378</h5>
+      </Popover.Body>
+    </Popover>
+  );
+  const Example = () => (
+    <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+      <Button variant="warning">Buy me</Button>
+    </OverlayTrigger>
+  );
   return (
     <div className="bloglist d-grid gap-2">
       <Container>
@@ -23,9 +37,7 @@ const Blog11 = ({ baby }) => {
           <Col sm={8} md={8}>
             <h1 className="items-name">{product.name}</h1>
             <p>{product.price}</p>
-            <Button variant="primary" size="lg">
-              Buy Me{" "}
-            </Button>
+            <Example />
           </Col>
         </Row>
       </Container>
