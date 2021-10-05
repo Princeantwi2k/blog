ed959837ead99a0fa3525800a043fa9fa9711236
princeantwi2k <antwi2k@gmail.com>
2021-10-05 15:23:22 +0800

    added AccessAction.js

--- src/Action/AccessAction.js
+++ src/Action/AccessAction.js
@@ -1,8 +1,15 @@
 export const ADD_ACCESS = "ADD_ACCESS"
+export const ADD_USER ="ADD_USER"
 
 export const PhoneAction =( accessories) =>{
     return {
         type: ADD_ACCESS,
         payload:accessories
+    }
+}
+export const AddAction =( user) =>{
+    return {
+        type:ADD_USER ,
+        payload:user
     }
 }
